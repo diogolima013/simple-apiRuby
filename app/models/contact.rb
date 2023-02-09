c925180a5f59687f127d91b1313ec8235fcaee79
Diogo <diogoslva0133@gmail.com>
2023-02-09 10:04:30 -0300

    add: I18n

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,18 @@
 class Contact < ApplicationRecord
   belongs_to :kind
 
+  # def birthdate_br
+  #   I18n.l(self.birthdate) unless self.birthdate.blank?
+  # end
+
+  def to_br
+    {
+      name: self.name, 
+      email: self.email,
+      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    }   
+  end
+
   # def author
   #   "Jackson Pires"
   # end
